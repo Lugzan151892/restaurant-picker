ccbf92ae995c945d51962fa255ebfb08a85018c9
Denis Gukov <denis.guk@konomic.com>
2024-06-10 21:03:30 +0300

    restaurants: added food types

--- src/views/restaurants/restaurants/RestaurantsView.vue
+++ src/views/restaurants/restaurants/RestaurantsView.vue
@@ -16,16 +16,38 @@
 				/>
 			</template>
 		</RInput>
-		<div>
-			<h3>Выбери свой тип</h3>
-			<div>тут список</div>
+		<div :class="$style[`${className}-types`]">
+			<h3
+				:class="$style[`${className}-types--title`]"
+				class="r-mt-16 r-mb-16"
+			>
+				Выбери свой тип
+			</h3>
+			<div
+				class="r-grid"
+				:class="$style[`${className}-types--list`]"
+			>
+				<RestaurantTypeItem
+					v-for="item in foodTypeList"
+					:key="item.id"
+					:text="item.text"
+					:background="item.background"
+					:image="item.image"
+				/>
+			</div>
 		</div>
 	</section>
 </template>
 <script lang="ts" setup>
+import { computed, ref } from 'vue';
 import RInput from '@/components/ui/RInput.vue';
 import searchIcon from '@/assets/icons/search.svg';
-import { computed, ref } from 'vue';
+import RestaurantTypeItem from '@/views/restaurants/restaurants/components/RestaurantTypeItem.vue';
+import fastFood from '@/assets/images/food_background/fastfood.jpg';
+import italian from '@/assets/images/food_background/italian.jpg';
+import kitaiskay from '@/assets/images/food_background/kitaiskaya_kuhnya.jpg';
+import korean from '@/assets/images/food_background/korean.jpg';
+import normalFood from '@/assets/images/food_background/normal_food.jpg';
 
 const className = 'restaurants-view';
 
@@ -35,32 +57,32 @@ const foodTypeList = computed(() => [
 	{
 		id: 1,
 		text: 'Фастфуд',
-		color: '',
-		image: '',
+		background: '#ff8a00',
+		image: fastFood,
 	},
 	{
 		id: 2,
 		text: 'Полезная пища',
-		color: '',
-		image: '',
+		background: '#ffb800',
+		image: normalFood,
 	},
 	{
 		id: 3,
 		text: 'Китайская кухня',
-		color: '',
-		image: '',
+		background: '#7a000',
+		image: kitaiskay,
 	},
 	{
 		id: 4,
 		text: 'Корейская кухня',
-		color: '',
-		image: '',
+		background: '#ff8a00',
+		image: korean,
 	},
 	{
 		id: 5,
 		text: 'Итальянская кухня',
-		color: '',
-		image: '',
+		background: '#ffb800',
+		image: italian,
 	},
 ]);
 </script>
@@ -68,5 +90,25 @@ const foodTypeList = computed(() => [
 $component: restaurants-view;
 
 .#{$component} {
+	&-types {
+		&--title {
+			font-size: 16px;
+			font-weight: 600;
+		}
+
+		&--list {
+			align-items: center;
+			grid-auto-flow: column;
+			column-gap: 8px;
+			grid-auto-columns: 120px;
+			overflow-x: scroll;
+			-ms-overflow-style: none;
+			scrollbar-width: none;
+		}
+
+		&--list::-webkit-scrollbar {
+			display: none;
+		}
+	}
 }
 </style>
