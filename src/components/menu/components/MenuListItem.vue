8f02a7623ad26f270b554d29cd148f2115485b7e
Denis Gukov <denis.gukov@allio.ru>
2024-06-10 12:53:37 +0300

    menu: added menu component, header component, menu link component, added styles

---
+++ src/components/menu/components/MenuListItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :class="$style[className]" class="r-grid">
+    <img class="r-h-35 r-w-35" :src="icon" :alt="text" />
+    <p :class="$style[`${className}-text`]">{{ text }}</p>
+  </div>
+</template>
+<script lang="ts" setup>
+  import noImage from '@/assets/icons/no-image.svg';
+
+  const componentName = 'MenuListItem';
+  const className = 'menu-list-item';
+
+  withDefaults(defineProps<{
+    icon?: string;
+    text?: string
+  }>(), {
+    text: '',
+    icon: noImage
+  });
+  
+</script>
+<style lang="scss" module>
+$component: menu-list-item;
+.#{$component} {
+  grid-template-columns: 35px max-content;
+  align-items: center;
+  column-gap: 24px;
+
+  &-text {
+    font-size: 20px;
+  }
+}
+</style>
---
+++ src/components/ui/RIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <img :src="src" :alt="alt" />
+</template>
+<script lang="ts" setup>
+
+  withDefaults(defineProps<{
+    src: string;
+    alt?: string;
+  }>(), {
+    alt: 'Icon'
+  })
+  const componentName = 'RIcon';
+  const className = 'r-icon';
+  
+</script>
+<style lang="scss" module>
+  $component: r-icon;
+  .#{$component} {
+    
+  }
+</style>
